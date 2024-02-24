5581c256a6d294a26318c4caa90c2f55624e967e
vslayer34 <mohamd.elnagar.34@gmail.com>
2024-02-24 05:28:33 +0200

    003 Player Movement
    Added movement to the player script

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,28 +1,18 @@
 extends CharacterBody2D
 
+var speed: float = 100.0
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
-
-
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+func _physics_process(_delta: float) -> void:
+	# Reset the velocity vector when the player stop inout actions
+	velocity = Vector2.ZERO
+	
+	if Input.is_action_pressed("move_right"):
+		velocity.x = speed
+	if Input.is_action_pressed("move_left"):
+		velocity.x = -speed
+	if Input.is_action_pressed("move_up"):
+		velocity.y = -speed
+	if Input.is_action_pressed("move_down"):
+		velocity.y = speed
+	
 	move_and_slide()
